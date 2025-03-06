0e0039df1ea009db3100effe3fff06ce347b46d3
acostarigoberti209 <acostarigoberti209@gmail.com>
2025-03-06 18:32:57 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,9 @@
+// School Project on Github
+export const studentName = {
+  firstName: "John",
+  lastName: "Doe"
+};
+
+export function getFullName(studentName) {
+  return `${studentName.firstName} ${studentName.lastName}`;
+}
