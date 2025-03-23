3ace96454f2938b22a28b1033cf9d948a7c3055a
acostarigoberti209 <acostarigoberti209@gmail.com>
2025-03-23 10:12:40 +0000

    Add 19.ts

---
+++ src/19.ts
@@ -0,0 +1,10 @@
+class SimpleSchoolProject {
+    constructor(name: string) {
+        this.name = name;
+        console.log(`[SimpleSchoolProject] ${name}`);
+    }
+}
+
+const project = new SimpleSchoolProject("Your School Project Name");
+
+console.log("[SimpleSchoolProject]");
