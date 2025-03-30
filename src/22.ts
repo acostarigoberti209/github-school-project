1cdd91c56326ddc9cc9d9cc6081fe12ce640296e
acostarigoberti209 <acostarigoberti209@gmail.com>
2025-03-30 21:23:39 +0000

    Add 22.ts

---
+++ src/22.ts
@@ -0,0 +1,16 @@
+let count = 0;
+
+function calculateSum(a: number, b: number): number {
+    if (a === b) {
+        count++;
+    }
+    return sum + b;
+}
+
+function sum(x: number): number {
+    if (x >= 10) {
+        return x * 2;
+    } else {
+        return x;
+    }
+}
